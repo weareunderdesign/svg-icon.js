b87857406de346d0180b778908a9989adf09f9a9
Talent <toptalent0411@gmail.com>
2022-10-21 09:53:21 -0400

    Optimized Package and Created Npx

--- bundler.config.js
+++ bundler.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './svg-icon.js',
+  entry: './src/svg-icon.js',
   output: {
     filename: 'svg-icon.min.js',
     path: path.resolve(__dirname, 'dist'),
---
+++ src/index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const { exec } = require("child_process");
+
+// install iconsvgjs package as dependency
+exec("npm install --save iconsvgjs", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+
+  // install degit package as dev-dependency
+  exec("npm install --save-dev degit", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+
+    // download the template structure of iconsvgjs
+    exec("degit toptalent0411/svg-icon-template --force", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  });
+});
+
