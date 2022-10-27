f2ab81441230ffd99024dfbbca4d06e49d9cf2e1
Talent <toptalent0411@gmail.com>
2022-10-27 14:57:20 -0400

    bug fix

--- src/svg-icon.js
+++ src/svg-icon.js
@@ -26,18 +26,20 @@ window.svgIcon = {
 
   checkReady(function ($) {
     $(function () {
-      // get current html href
-      let currentHtmlHref = window.location.href
+      $(document).on('ready', () => {
+        // get current html href
+        let currentHtmlHref = window.location.href
 
-      // get configFileData
-      findConfig(currentHtmlHref)
-        .then((configFileData) => {
-          if (configFileData) {
-            window.svgIcon.config = configFileData
-          }
-          // define my custom element <svg-icon></svg-icon>
-          defineSvgIcon(window.svgIcon.config)
-        })
+        // get configFileData
+        findConfig(currentHtmlHref)
+          .then((configFileData) => {
+            if (configFileData) {
+              window.svgIcon.config = configFileData
+            }
+            // define my custom element <svg-icon></svg-icon>
+            defineSvgIcon(window.svgIcon.config)
+          })
+      })
     })
   })
 })()
