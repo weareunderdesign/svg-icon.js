1e6baf6bb18cd055fc890e85fbdfe6cf15b572eb
Talent <toptalent0411@gmail.com>
2022-10-27 15:46:31 -0400

    svg-icon.js 1.2.4

--- src/svg-icon.js
+++ src/svg-icon.js
@@ -30,23 +30,21 @@ window.svgIcon = {
   }
 
   checkReady(function ($) {
-    $(document).on('ready', () => {
-      console.log('svg-icon.js document ready');
-      $(function () {
-        // get current html href
-        let currentHtmlHref = window.location.href
+    $(function () {
+      // get current html href
+      let currentHtmlHref = window.location.href
 
-        // get configFileData
-        findConfig(currentHtmlHref)
-          .then((configFileData) => {
-            if (configFileData) {
-              window.svgIcon.config = configFileData
-            }
-            // define my custom element <svg-icon></svg-icon>
-            defineSvgIcon(window.svgIcon.config)
-          })
-      })
+      // get configFileData
+      findConfig(currentHtmlHref)
+        .then((configFileData) => {
+          if (configFileData) {
+            window.svgIcon.config = configFileData
+          }
+          // define my custom element <svg-icon></svg-icon>
+          defineSvgIcon(window.svgIcon.config)
+        })
     })
+    console.log('svg-icon.js end');
   })
 })()
 
